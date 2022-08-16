45e684c906e11f1416281f7ffb49b16593d1be37
陈沫言 <ab112334@163.com>
2022-08-16 10:33:40 +0800

    开始页图片

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -2,7 +2,11 @@ import { useNavigate } from "react-router-dom";
 
 const Start = () => {
     const navigate = useNavigate();
-    return <div onClick={() => navigate("/home")}>Start</div>;
+    return (
+        <div onClick={() => navigate("/home")}>
+            <img width="100%" src="./imgs/start.jpg" />
+        </div>
+    );
 };
 
 export default Start;
