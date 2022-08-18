5ede1d2f36330de4ebdf083a0387436709d693d1
陈沫言 <ab112334@163.com>
2022-08-18 16:24:20 +0800

    隐藏菜单

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,33 +1,45 @@
 import { Layout, Menu } from "antd";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./index.css";
 
 const GraduationLayout = ({ children }) => {
     const location = useLocation();
+    const [menuHidden, setMenuHidden] = useState(false);
     const layout = (
         <>
             <Layout>
-                <Menu
-                    style={{ background: "#cdd4a6" }}
-                    mode="horizontal"
-                    defaultSelectedKeys={[location.pathname]}
-                >
-                    <Menu.Item style={{ left: "10px" }} key="/">
-                        <Link className="menu-link" to={"/home"}>
-                            Home
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item style={{ position: "absolute", right: "120px" }} key="/class1">
-                        <Link className="menu-link" to={"/class1"}>
-                            Class1
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item style={{ position: "absolute", right: "10px" }} key="/class2">
-                        <Link className="menu-link" to={"/class2"}>
-                            Class2
-                        </Link>
-                    </Menu.Item>
-                </Menu>
+                {menuHidden ? null : (
+                    <Menu
+                        style={{ background: "#cdd4a6" }}
+                        mode="horizontal"
+                        defaultSelectedKeys={[location.pathname]}
+                    >
+                        <Menu.Item style={{ left: "10px" }} key="/home">
+                            <Link className="menu-link" to={"/home"}>
+                                Home
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item style={{ position: "absolute", right: "120px" }} key="/class1">
+                            <Link className="menu-link" to={"/class1"}>
+                                Class1
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item style={{ position: "absolute", right: "10px" }} key="/class2">
+                            <Link className="menu-link" to={"/class2"}>
+                                Class2
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                )}
+                <div style={{ background: "#cdd4a6" }}>
+                    <div
+                        className="menu-hidden-bar"
+                        onClick={() => {
+                            setMenuHidden(!menuHidden);
+                        }}
+                    ></div>
+                </div>
             </Layout>
             {children}
             <div style={{ height: "200px", "background-color": "#cdd4a6" }}></div>
