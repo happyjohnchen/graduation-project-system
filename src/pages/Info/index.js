1516741d657b54d753a712291d5ee2cfc666d7da
陈沫言 <ab112334@163.com>
2022-09-04 13:04:06 +0800

    完善详情页

--- src/pages/Info/index.js
+++ src/pages/Info/index.js
@@ -7,7 +7,7 @@ const Info = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const id = parseInt(location.pathname.split("/").pop());
-    const [project, setProject] = useState({});
+    const [project, setProject] = useState({ images: [] });
     useEffect(() => {
         Utils.getData("/works/result.json", (datas) => {
             for (const p of datas.list) {
@@ -24,30 +24,48 @@ const Info = () => {
     return (
         <div>
             <div className="info-card">
-                <div className="info-card-title">作者图片</div>
-                <div className="info-card-content">这是是作者的照片</div>
-            </div>
-            <div className="info-card">
-                <div className="info-card-title">作者介绍</div>
-                <div className="info-card-content">
-                    {project.name + ", " + project.class + "班"}
-                    <br />
-                    这里是作者的介绍
-                </div>
-            </div>
-            <div className="info-card">
-                <div className="info-card-title">视频</div>
+                <div className="info-card-title">作品图片</div>
                 <div className="info-card-content">
                     <img className="info-image" src={project.image} alt={project.projectTitle} />
                 </div>
             </div>
             <div className="info-card">
+                <div className="info-card-title">作者介绍</div>
+                <div className="info-card-content">
+                    <div className="info-intro">
+                        姓名: {project.name}
+                        <br />
+                        班级: 1908121{project.class}班
+                        <br />
+                        专业: 数字媒体技术
+                        <br />
+                        指导老师: {project.teacher}
+                    </div>
+                </div>
+            </div>
+            <div className="info-card">
+                <div className="info-card-title">视频</div>
+                <div className="info-card-content">
+                    <video className="info-video" src={project.video} controls={true} />
+                </div>
+            </div>
+            <div className="info-card">
                 <div className="info-card-title">作品简介</div>
-                <div className="info-card-content">{project.info}</div>
+                <div className="info-card-content">
+                    <div className="info-project">
+                        作品题目: {project.projectTitle}
+                        <br />
+                        作品综述: {project.info}
+                    </div>
+                </div>
             </div>
             <div className="info-detail">
                 <div style={{ fontSize: "xx-large" }}>作品详情</div>
-                <div className="info-card-detail">{project.detail}</div>
+                <div className="info-card-detail">
+                    {project.images.map((item) => (
+                        <img className="info-detail-image" src={item} />
+                    ))}
+                </div>
             </div>
         </div>
     );
